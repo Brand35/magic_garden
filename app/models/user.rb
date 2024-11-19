93fa5bf3df957483e09459f2742f0fae9b439aca
Sidney <sidney.nguyen@live.fr>
2024-11-19 22:58:16 +0100

    controller booking terminé

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :bookings
-  has_many :items
+  has_many :bookings, foreign_key: :renter_id
+  has_many :items, foreign_key: :owner_id
   has_many :bookings_as_owner, through: :items, source: :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
