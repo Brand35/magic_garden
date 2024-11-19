bf6f85994230703da1dbe0330f55497bf3270867
May Ya <maysoua.y@gmail.com>
2024-11-19 12:58:16 +0100

    merging 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.2].define(version: 2024_11_19_105248) do
-=======
 ActiveRecord::Schema[7.2].define(version: 2024_11_19_105739) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
