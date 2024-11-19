551f70c717397127265f14e30533163294a7b14c
May Ya <maysoua.y@gmail.com>
2024-11-19 12:45:05 +0100

    Seed en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 1. Clean the database 🗑️
+puts "Cleaning database..."
+Item.destroy_all
+User.destroy_all
+
+# 2. Create the instances of items 🏗️
+puts "Creating X items..."
+Item.create(name: "", description: "", available: "", price: 10, owner_id: )
+puts "Created "
+Item.create(name: "", description: "", available: "", price: 10, owner_id: )
+puts "Created "
+Item.create(name: "", description: "", available: "", price: 10, owner_id: )
+puts "Created "
+Item.create(name: "", description: "", available: "", price: 10, owner_id: )
+puts "Created "
+Item.create(name: "", description: "", available: "", price: 10, owner_id: )
+puts "Created "
+Item.create(name: "", description: "", available: "", price: 10, owner_id: )
+puts "Created "
+
+# 3. Create the instances of users (owner/renter)
+puts "Creating Users..."
+User.create(email: "", password: "", first_name: "", last_name: "")
+User.create(email: "", password: "", first_name: "", last_name: "")
+User.create(email: "", password: "", first_name: "", last_name: "")
+User.create(email: "", password: "", first_name: "", last_name: "")
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Item.count} items and #{User.count} users."
